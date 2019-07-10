f0c5f45c78d38dd74cdd51975993b75bb1b7e85c
Ewelina Łodyńska <ew.krasowska@hotmail.com>
2019-07-10 13:55:48 +0200

    implement interfaces for services Closes #5

---
+++ pet-clinic-data/src/main/java/com/ewelina/petclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package com.ewelina.petclinic.services;
+
+import com.ewelina.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/ewelina/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package com.ewelina.petclinic.services;
+
+import com.ewelina.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+
+}
