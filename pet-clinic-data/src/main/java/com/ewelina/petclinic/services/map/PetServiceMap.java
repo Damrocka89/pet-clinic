67118375c0205e673713138c75d884511146244e
ewkra <EW.KRASOWSKA@HOTMAIL.COM>
2019-07-12 09:35:50 +0200

    adding bootstrap for data. closes #13

--- pet-clinic-data/src/main/java/com/ewelina/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/ewelina/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.ewelina.petclinic.services.map;
 
 import com.ewelina.petclinic.model.Pet;
-import com.ewelina.petclinic.services.CrudService;
+import com.ewelina.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
