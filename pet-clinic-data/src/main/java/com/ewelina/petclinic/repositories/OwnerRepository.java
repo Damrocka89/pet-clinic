76fe9ccb44dcd5c354229b48b4cfd8f4a9c50f67
ewkra <EW.KRASOWSKA@HOTMAIL.COM>
2019-07-18 17:01:08 +0200

    added spring data jpa service for owners

--- pet-clinic-data/src/main/java/com/ewelina/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/ewelina/petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.ewelina.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastname);
 }
