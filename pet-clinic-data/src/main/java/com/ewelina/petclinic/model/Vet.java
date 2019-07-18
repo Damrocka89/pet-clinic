23232e31579f580869e59cb41f52e2b1a0e8be4b
ewkra <EW.KRASOWSKA@HOTMAIL.COM>
2019-07-18 11:34:16 +0200

    implemeted vet, specialty entities

--- pet-clinic-data/src/main/java/com/ewelina/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ewelina/petclinic/model/Vet.java
@@ -1,11 +1,17 @@
 package com.ewelina.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
-    private Set<Specialty> specialties=new HashSet<>();
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
+    private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
         return specialties;
