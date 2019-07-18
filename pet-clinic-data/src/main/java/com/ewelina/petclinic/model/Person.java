ca7baf7620b990dc1ffb0958df519c1c6f9725da
ewkra <EW.KRASOWSKA@HOTMAIL.COM>
2019-07-18 11:21:37 +0200

    implemeted owner, pet, pet type entities

--- pet-clinic-data/src/main/java/com/ewelina/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/ewelina/petclinic/model/Person.java
@@ -1,8 +1,15 @@
 package com.ewelina.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    //hibernate would name the column like that anyway - annotation column here only for learning purposes
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
