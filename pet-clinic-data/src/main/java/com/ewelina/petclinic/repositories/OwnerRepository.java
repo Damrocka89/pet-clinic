541b58025dd82934c37563d16f1a0fb02fd8059c
ewkra <EW.KRASOWSKA@HOTMAIL.COM>
2019-08-20 11:19:13 +0200

    find owners with wildcards added

--- pet-clinic-data/src/main/java/com/ewelina/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/ewelina/petclinic/repositories/OwnerRepository.java
@@ -3,6 +3,10 @@ package com.ewelina.petclinic.repositories;
 import com.ewelina.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastname);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/ewelina/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/ewelina/petclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package com.ewelina.petclinic.services;
 
 import com.ewelina.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
